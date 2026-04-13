80ff749e2657ba00398fc5b2e318da30b7c95589
Chris Scheu <chris.scheu@dfamilk.com>
2026-04-13 15:14:53 -0500

    updated PowerApps cli script installation; added cr113_jde_co_assignment to the datasources

---
+++ .power/schemas/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "cr113_jde_co_assignments": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr113_jde_co_assignmentid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  }
+};
